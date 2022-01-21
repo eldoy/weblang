30efacb07e6c16f279b6623306bc96adc90dbb67
Vidar Eldøy <vidar@eldoy.com>
2022-01-21 10:15:03 +0100

    Move tests

---
+++ spec/tests/set-test.js
@@ -0,0 +1,161 @@
+const weblang = require('../../index.js')
+
+it('should set string variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello: world'
+  ].join('\n'))
+  t.ok(state.vars.hello == 'world')
+})
+
+it('should set object variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  n: 0'
+  ].join('\n'))
+  t.ok(state.vars.hello.n == 0)
+})
+
+it('should set array variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  - 1',
+    '  - 2'
+  ].join('\n'))
+  t.ok(state.vars.hello[0] == 1)
+  t.ok(state.vars.hello[1] == 2)
+})
+
+it('should set bool variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello: true'
+  ].join('\n'))
+  t.ok(state.vars.hello === true)
+})
+
+it('should set variable from other variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello: world',
+    '$bye: $hello'
+  ].join('\n'))
+  t.ok(state.vars.hello == 'world')
+  t.ok(state.vars.bye == 'world')
+})
+
+it('should set object value from other variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello: world',
+    '$bye:',
+    '  name: $hello'
+  ].join('\n'))
+  t.ok(state.vars.hello == 'world')
+  t.ok(state.vars.bye.name == 'world')
+})
+
+it('should set array index from other variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello: world',
+    '$bye:',
+    '  - $hello'
+  ].join('\n'))
+  t.ok(state.vars.hello == 'world')
+  t.ok(state.vars.bye[0] == 'world')
+})
+
+it('should set variable with object dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  name:',
+    '    deep: 1',
+    '$bye: $hello.name.deep'
+  ].join('\n'))
+  t.ok(state.vars.bye == 1)
+})
+
+it('should set variable with array dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  - 1',
+    '  - 2',
+    '$bye: $hello[0]'
+  ].join('\n'))
+  t.ok(state.vars.bye == 1)
+})
+
+it('should set object field variable, dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello.name: nils'
+  ].join('\n'))
+  t.ok(state.vars.hello.name == 'nils')
+})
+
+it('should set object field variable deep, dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello.name.deep: nils'
+  ].join('\n'))
+  t.ok(state.vars.hello.name.deep == 'nils')
+})
+
+it('should update object field variable, dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello.name: nils',
+    '$hello.name: kari'
+  ].join('\n'))
+  t.ok(state.vars.hello.name == 'kari')
+})
+
+it('should set variable with array object dot notation', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  - name: nils',
+    '$bye: $hello[0].name'
+  ].join('\n'))
+  t.ok(state.vars.bye == 'nils')
+})
+
+it('should set empty string if variable does not exist', async ({ t }) => {
+  const state = await weblang()([
+    '$bye: $hello'
+  ].join('\n'))
+  t.ok(state.vars.bye == '')
+})
+
+it('should delete a variable', async ({ t }) => {
+  const state = await weblang()([
+    '$hello@1: world',
+    '$hello@2: null'
+  ].join('\n'))
+  t.ok(typeof state.vars.hello == 'undefined')
+})
+
+it('should delete a value from object', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  name: world',
+    '  email: a@world.no',
+    '$hello.email: null'
+  ].join('\n'))
+  t.ok(state.vars.hello.name == 'world')
+  t.ok(typeof state.vars.hello.email == 'undefined')
+})
+
+it('should delete a value from object', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  name: world',
+    '  email: a@world.no',
+    '$hello.email: null'
+  ].join('\n'))
+  t.ok(state.vars.hello.name == 'world')
+  t.ok(typeof state.vars.hello.email == 'undefined')
+})
+
+it('should delete an array index', async ({ t }) => {
+  const state = await weblang()([
+    '$hello:',
+    '  - a',
+    '  - b',
+    '$hello[0]: null'
+  ].join('\n'))
+  t.ok(state.vars.hello.length == 1)
+  t.ok(state.vars.hello[0] == 'b')
+})
