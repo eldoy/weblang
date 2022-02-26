6f9141859933af1a5917da0fe71b2fe0ef059421
Vidar Eldøy <vidar@eldoy.com>
2022-02-26 12:14:57 +0100

    Use # for internal id instead of @

--- spec/tests/util-test.js
+++ spec/tests/util-test.js
@@ -7,7 +7,7 @@ it('should split name without id', async ({ t }) => {
 })
 
 it('should split name with id', async ({ t }) => {
-  let result = util.split('hello@1234')
+  let result = util.split('hello#1234')
   t.ok(result[0] == 'hello')
   t.ok(result[1] == '1234')
 })
@@ -19,7 +19,7 @@ it('should split name without id, dotted', async ({ t }) => {
 })
 
 it('should split name with id, dotted', async ({ t }) => {
-  let result = util.split('hello@1234.name.bye')
+  let result = util.split('hello#1234.name.bye')
   t.ok(result[0] == 'hello.name.bye')
   t.ok(result[1] == '1234')
 })
