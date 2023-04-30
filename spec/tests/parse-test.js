1a4cf1b63691946cff101199867bf1b2db684ede
Vidar Eldøy <vidar@eldoy.com>
2023-04-30 16:19:54 +0200

    Use space for pipe params

--- spec/tests/parse-test.js
+++ spec/tests/parse-test.js
@@ -1,7 +1,7 @@
 const parse = require('../../lib/parse.js')
 
 it('should parse a statement into value and pipes', async ({ t }) => {
-  let [val, pipes] = parse('hello | join hello=1234&name=bye | something b=1')
+  let [val, pipes] = parse('hello | join hello=1234 name=bye | something b=1')
   t.ok(val, 'hello')
   t.ok(pipes['join'].hello == '1234')
   t.ok(pipes['join'].name == 'bye')
