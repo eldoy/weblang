d121b802a4ed8dbc5e7437d1ff372485456f7716
Vidar Eldøy <vidar@eldoy.com>
2023-04-30 15:45:23 +0200

    Use real id in tests

--- spec/tests/util-test.js
+++ spec/tests/util-test.js
@@ -7,9 +7,9 @@ it('should split name without id', async ({ t }) => {
 })
 
 it('should split name with id', async ({ t }) => {
-  let result = util.split('hello#1234')
+  let result = util.split('hello#sdiskr1qhwuci4baas2xz9zo')
   t.ok(result[0] == 'hello')
-  t.ok(result[1] == '1234')
+  t.ok(result[1] == 'sdiskr1qhwuci4baas2xz9zo')
 })
 
 it('should split name without id, dotted', async ({ t }) => {
@@ -19,7 +19,7 @@ it('should split name without id, dotted', async ({ t }) => {
 })
 
 it('should split name with id, dotted', async ({ t }) => {
-  let result = util.split('hello#1234.name.bye')
+  let result = util.split('hello#sdiskr1qhwuci4baas2xz9zo.name.bye')
   t.ok(result[0] == 'hello.name.bye')
-  t.ok(result[1] == '1234')
+  t.ok(result[1] == 'sdiskr1qhwuci4baas2xz9zo')
 })
