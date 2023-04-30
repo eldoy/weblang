ce7aa7b32f8f36e90dc3c8e4cdcfc3bfc0124abd
Vidar Eldøy <vidar@eldoy.com>
2023-04-30 16:02:20 +0200

    Extract ext from util split

--- spec/tests/util-test.js
+++ spec/tests/util-test.js
@@ -1,25 +1,57 @@
 const util = require('../../lib/util.js')
 
 it('should split name without id', async ({ t }) => {
-  let result = util.split('hello')
-  t.ok(result[0] == 'hello')
-  t.ok(result[1] == '')
+  let [key, ext, id] = util.split('=hello')
+  t.ok(key == '=hello')
+  t.ok(ext == '')
+  t.ok(id == '')
 })
 
 it('should split name with id', async ({ t }) => {
-  let result = util.split('hello#sdiskr1qhwuci4baas2xz9zo')
-  t.ok(result[0] == 'hello')
-  t.ok(result[1] == 'sdiskr1qhwuci4baas2xz9zo')
+  let [key, ext, id] = util.split('=hello#sdiskr1qhwuci4baas2xz9zo')
+  t.ok(key == '=hello')
+  t.ok(ext == '')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
 })
 
 it('should split name without id, dotted', async ({ t }) => {
-  let result = util.split('hello.name.bye')
-  t.ok(result[0] == 'hello.name.bye')
-  t.ok(result[1] == '')
+  let [key, ext, id] = util.split('=hello.name.bye')
+  t.ok(key == '=hello.name.bye')
+  t.ok(ext == '')
+  t.ok(id == '')
 })
 
 it('should split name with id, dotted', async ({ t }) => {
-  let result = util.split('hello#sdiskr1qhwuci4baas2xz9zo.name.bye')
-  t.ok(result[0] == 'hello.name.bye')
-  t.ok(result[1] == 'sdiskr1qhwuci4baas2xz9zo')
+  let [key, ext, id] = util.split('=hello#sdiskr1qhwuci4baas2xz9zo.name.bye')
+  t.ok(key == '=hello.name.bye')
+  t.ok(ext == '')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
+})
+
+it('should split name with key, ext and id', async ({ t }) => {
+  let [key, ext, id] = util.split('=hello#sdiskr1qhwuci4baas2xz9zo@ext')
+  t.ok(key == '=hello')
+  t.ok(ext == 'ext')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
+})
+
+it('should split name with only ext and id', async ({ t }) => {
+  let [key, ext, id] = util.split('@ext#sdiskr1qhwuci4baas2xz9zo')
+  t.ok(key == '')
+  t.ok(ext == 'ext')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
+})
+
+it('should split name with key, ext and id, dotted', async ({ t }) => {
+  let [key, ext, id] = util.split('=hello#sdiskr1qhwuci4baas2xz9zo.name.bye@ext')
+  t.ok(key == '=hello.name.bye')
+  t.ok(ext == 'ext')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
+})
+
+it('should split name with key, ext and id, prespaced', async ({ t }) => {
+  let [key, ext, id] = util.split('  =hello#sdiskr1qhwuci4baas2xz9zo.name')
+  t.ok(key == '=hello.name')
+  t.ok(ext == '')
+  t.ok(id == 'sdiskr1qhwuci4baas2xz9zo')
 })
