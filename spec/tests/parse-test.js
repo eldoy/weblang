ecb17f012e394f049b53e5a212e387f331f9f059
Vidar Eldøy <vidar@eldoy.com>
2023-05-01 07:21:26 +0200

    Rename core to ext

--- spec/tests/parse-test.js
+++
@@ -1,9 +0,0 @@
-const parse = require('../../lib/parse.js')
-
-it('should parse a statement into value and pipes', async ({ t }) => {
-  let [val, pipes] = parse('hello | join hello=1234 name=bye | something b=1')
-  t.ok(val, 'hello')
-  t.ok(pipes['join'].hello == '1234')
-  t.ok(pipes['join'].name == 'bye')
-  t.ok(pipes['something'].b == '1')
-})
