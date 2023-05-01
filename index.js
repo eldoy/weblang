fce4fb1788d55cfe4998387fa46ec1c500634f11
Vidar Eldøy <vidar@eldoy.com>
2023-05-01 12:43:55 +0200

    Remove expand

--- index.js
+++ index.js
@@ -1,18 +1,93 @@
 const _ = require('lodash')
+const { transform, undot } = require('extras')
 const { validate } = require('d8a')
-const expand = require('./lib/expand.js')
 const load = require('./lib/load.js')
 const util = require('./lib/util.js')
 const ext = require('./lib/ext.js')
 const pipes = require('./lib/pipes.js')
 const renderers = require('./lib/renderers.js')
 
-module.exports = function(opt = {}) {
+function expand(obj = {}, state = {}, opt = {}) {
+  const wasString = typeof obj == 'string'
+  if (wasString) obj = [obj]
+
+  if (_.isPlainObject(obj)) {
+    obj = undot(_.cloneDeep(obj))
+  }
+
+  function build(obj, state, opt) {
+    for (const key in obj) {
+      if (obj[key] && typeof obj[key] == 'object') {
+        build(obj[key], state, opt)
+      } else if (typeof obj[key] == 'string') {
+        let statement = obj[key]
+
+        let [val, ...pipes] = statement.split('|').map((x) => x.trim())
+
+        let data = {}
+        for (const pipe of pipes) {
+          const [lang, body] = util.renderer(pipe)
+
+          if (body) {
+            data = body
+            if (lang) {
+              const renderer = opt.renderers[lang]
+              if (typeof renderer == 'function') {
+                // TODO: Async + pass args
+                data = renderer()
+              }
+            }
+          } else {
+            let [name, ...options] = pipe.split(' ').map((x) => x.trim())
+
+            const params = {}
+            for (const opt of options) {
+              let [key, val] = opt.split('=')
+              val = util.get(val, state)
+              params[key] = val
+            }
+            data[name] = params
+          }
+        }
+        pipes = data
+
+        val = util.get(val, state)
+
+        if (typeof pipes == 'string') {
+          val = pipes
+        } else {
+          val = transform(val)
+
+          // Apply pipes
+          for (const name in pipes) {
+            const pipe = (opt.pipes || {})[name]
+            if (typeof pipe == 'function') {
+              val = pipe(val, pipes[name])
+            }
+          }
+        }
+
+        // Remove undefined
+        if (typeof val == 'undefined') {
+          delete obj[key]
+        } else {
+          obj[key] = val
+        }
+      }
+    }
+  }
+
+  build(obj, state, opt)
+
+  return wasString ? obj[0] : obj
+}
+
+module.exports = function (opt = {}) {
   opt.pipes = { ...pipes, ...opt.pipes }
   opt.ext = { ...ext, ...opt.ext }
   opt.renderers = { ...renderers, ...opt.renderers }
 
-  return async function(code, params) {
+  return async function (code, params) {
     const state = {
       vars: {}
     }
@@ -39,7 +114,7 @@ module.exports = function(opt = {}) {
       for (const field in val) {
         const obj = val[field]
         const checks = get(field)
-        if (checks && await validate(obj, checks)) {
+        if (checks && (await validate(obj, checks))) {
           return false
         }
       }
