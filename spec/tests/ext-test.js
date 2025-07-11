42ed9bb4b22cb2185e00d06249f12c48ffc92fe5
Vidar Eldøy <vidar@eldoy.com>
2025-07-10 21:00:03 -0300

    feat: allow null vars, add delete ext to remove any var
    (change set tests, add new suits on ext tests)

--- spec/tests/ext-test.js
+++ spec/tests/ext-test.js
@@ -4,26 +4,66 @@ var { db } = require('../lib/ext.js')
 it('should support custom extensions', async ({ t }) => {
   var code = '@db: user/create'
   var state = await weblang.init({ ext: { db } }).run(code)
-  t.ok(state.vars.internal == 'hello')
+
+  t.equal(state.vars.internal, 'hello')
 })
 
 it('should set variable with extensions', async ({ t }) => {
   var code = ['=result@db: user/create', '@return: $result'].join('\n')
   var state = await weblang.init({ ext: { db } }).run(code)
-  t.ok(state.vars.result.id == '1')
-  t.ok(state.return.id == '1')
+
+  t.equal(state.vars.result.id, '1')
+  t.equal(state.return.id, '1')
 })
 
-it('should not set variable if undefined', async ({ t }) => {
+it('should throw error if no returned tuple', async ({ t }) => {
+  var hasError = false
+
+  var db = function ({ set }) {
+    set('result', 'hello')
+  }
+
   var code = ['=result@db: user/create', '@return: $result'].join('\n')
-  var state = await weblang
-    .init({
-      ext: {
-        db: function ({ set }) {
-          set('result', 'hello')
-        }
-      }
-    })
+  await weblang
+    .init({ ext: { db } })
     .run(code)
-  t.ok(state.vars.result == 'hello')
+    .catch(() => (hasError = true))
+
+  t.equal(hasError, true)
+})
+
+it('should delete a null variable', async ({ t }) => {
+  var code = ['=hello: world', '=hello: null', '@delete: $hello'].join('\n')
+  var state = await weblang.init().run(code)
+
+  t.ok(state.vars.hello === undefined)
+})
+
+it('should delete a null nested var', async ({ t }) => {
+  var code = ['=param:', ' hello: world', '@delete: $param.hello'].join('\n')
+  var state = await weblang.init().run(code)
+
+  t.ok(state.vars.param !== undefined)
+  t.ok(state.vars.param.hello === undefined)
+})
+
+it('should delete a null nested index', async ({ t }) => {
+  var code = ['=param:', ' list: [1, 2]', '@delete: $param.list[0]'].join('\n')
+  var state = await weblang.init().run(code)
+
+  t.ok(state.vars.param.list.length === 1)
+  t.ok(state.vars.param.list[0] === 2)
+})
+
+it('should delete a null nested index var', async ({ t }) => {
+  var code = [
+    '=list: ',
+    ' - name: john',
+    '   surname: smith',
+    '@delete: $list[0].name'
+  ].join('\n')
+  var state = await weblang.init().run(code)
+
+  t.ok(state.vars.list[0].name === undefined)
+  t.ok(state.vars.list[0].surname === 'smith')
 })
