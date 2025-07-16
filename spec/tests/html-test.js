24a7f1e0c48030c17e67dbbe7b338d9ea3399a9b
Vidar Eldøy <vidar@eldoy.com>
2025-07-15 21:24:01 -0300

    feat: process sibling tags and tags with attrs, add test

--- spec/tests/html-test.js
+++ spec/tests/html-test.js
@@ -2,7 +2,6 @@ var weblang = require('../../index.js')
 var { div } = require('../../lib/html.js')
 
 test('simple div', async ({ t }) => {
-  // String content
   var code = '@div: hello'
   var state = await weblang.init({ ext: { div } }).run(code)
 
@@ -11,4 +10,41 @@ test('simple div', async ({ t }) => {
   t.equal(state.vars.tags[0].children.length, 1)
   t.equal(state.vars.tags[0].children[0].type, 'text')
   t.equal(state.vars.tags[0].children[0].content, 'hello')
+})
+
+test('multi div', async ({ t }) => {
+  var code = ['@div: hello', '@div: bye'].join('\n')
+  var state = await weblang.init({ ext: { div } }).run(code)
+
+  t.equal(state.vars.tags.length, 2)
+
+  t.equal(state.vars.tags[0].type, 'element')
+  t.equal(state.vars.tags[0].tagName, 'div')
+  t.equal(state.vars.tags[0].children.length, 1)
+  t.equal(state.vars.tags[0].children[0].type, 'text')
+  t.equal(state.vars.tags[0].children[0].content, 'hello')
+
+  t.equal(state.vars.tags[1].type, 'element')
+  t.equal(state.vars.tags[1].tagName, 'div')
+  t.equal(state.vars.tags[1].children.length, 1)
+  t.equal(state.vars.tags[1].children[0].type, 'text')
+  t.equal(state.vars.tags[1].children[0].content, 'bye')
+})
+
+test('simple attributes', async ({ t }) => {
+  var code = '@div: { id: 123, class: hello, text: bye }'
+  var state = await weblang.init({ ext: { div } }).run(code)
+
+  t.equal(state.vars.tags[0].type, 'element')
+  t.equal(state.vars.tags[0].tagName, 'div')
+
+  t.equal(state.vars.tags[0].attributes.length, 2)
+  t.equal(state.vars.tags[0].attributes[0].key, 'id')
+  t.equal(state.vars.tags[0].attributes[0].value, '123')
+  t.equal(state.vars.tags[0].attributes[1].key, 'class')
+  t.equal(state.vars.tags[0].attributes[1].value, 'hello')
+
+  t.equal(state.vars.tags[0].children.length, 1)
+  t.equal(state.vars.tags[0].children[0].type, 'text')
+  t.equal(state.vars.tags[0].children[0].content, 'bye')
 })
