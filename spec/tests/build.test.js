0b2b520b20cbbafb7d7697badfd7562a93ba3b5d
Vidar Eldøy <vidar@eldoy.com>
2025-08-15 10:36:27 -0300

    feat: combine build, shape to get AST upon IRT

--- spec/tests/build.test.js
+++ spec/tests/build.test.js
@@ -14,15 +14,34 @@ test('correct order', async ({ t }) => {
     keysVisited.push(key)
   })
 
-  // Test expected DFS post-order:
-  t.equal(keysVisited[0], 'a1')
-  t.equal(keysVisited[1], 'a2')
-  t.equal(keysVisited[2], 'a')
-  t.equal(keysVisited[3], 'b11')
+  t.equal(keysVisited[0], 'a')
+  t.equal(keysVisited[1], 'a1')
+  t.equal(keysVisited[2], 'a2')
+  t.equal(keysVisited[3], 'b')
   t.equal(keysVisited[4], 'b1')
-  t.equal(keysVisited[5], 'b')
-  t.equal(keysVisited[6], 'c1')
-  t.equal(keysVisited[7], 'c2')
-  t.equal(keysVisited[8], 'c')
+  t.equal(keysVisited[5], 'b11')
+  t.equal(keysVisited[6], 'c')
+  t.equal(keysVisited[7], 'c1')
+  t.equal(keysVisited[8], 'c2')
   t.equal(keysVisited[9], 'd')
+})
+
+test('assign', async ({ t }) => {
+  var irt = { '=hello_ID_s-1-1-1_ID_': 'user' }
+  var result = build(irt)
+
+  t.equal(result.id, 's-1-1-1')
+  t.equal(result.key, 'hello')
+  t.equal(result.value, 'user')
+})
+
+test('assign children', async ({ t }) => {
+  var irt = { '@p_ID_s-1-1-1_ID_': { '@p_ID_s-1-1-2_ID_': 'a' } }
+  var result = build(irt)
+
+  t.equal(result.id, 's-1-1-1')
+  t.equal(result.key, '@p')
+  t.equal(result.children[0].key, '@p')
+  t.equal(result.children[0].id, 's-1-1-2')
+  t.equal(result.children[0].value, 'a')
 })
