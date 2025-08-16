0196e117350483d95fc37eecb04fdbf3f45cd7ce
Vidar Eldøy <vidar@eldoy.com>
2025-08-16 16:05:24 +0200

    fix: update build test

--- spec/tests/build.test.js
+++ spec/tests/build.test.js
@@ -1,22 +1,14 @@
 var build = require('../../lib/build.js')
 
-test('returns root keys in order', ({ t }) => {
-  var obj = { a: 1, b: 2, c: 3 }
-  var keysVisited = []
+test('returns root nodes in order', ({ t }) => {
+  var tree = { a: 1, b: 2, c: 3 }
+  var visited = []
 
-  build(obj, (key, value) => {
-    keysVisited.push(key)
+  build(tree, (node) => {
+    visited.push(node)
   })
 
-  t.equal(keysVisited[0], 'a')
-  t.equal(keysVisited[1], 'b')
-  t.equal(keysVisited[2], 'c')
-})
-
-test('returns only first node', ({ t }) => {
-  var obj = { a: 1, b: 2, c: 3 }
-  var result = build(obj, (key, value) => ({ key, value }))
-
-  t.equal(result.key, 'a')
-  t.equal(result.value, 1)
+  t.equal(visited[0], 1)
+  t.equal(visited[1], 2)
+  t.equal(visited[2], 3)
 })
