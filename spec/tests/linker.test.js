e52a7552680baf0e36d01cd95c6bac3e2ebb51df
Vidar Eldøy <vidar@eldoy.com>
2025-08-19 04:30:39 +0200

    fix: linker should return array

--- spec/tests/linker.test.js
+++ spec/tests/linker.test.js
@@ -1,245 +1,253 @@
 var linker = require('../../lib/linker.js')
 
-test('object', async ({ t }) => {
-  var node = null
-  var result = linker(node)
-  t.equal(result, null)
+test('empty', async ({ t }) => {
+  var ast = []
+  var result = linker(ast)
+  t.deepEqual(result, [])
 })
 
 test('no children', async ({ t }) => {
-  var node = {
-    id: 's-1-1-1',
-    key: 'hello',
-    value: 'user',
-    mode: 'sync',
-    block: 1,
-    line: 1,
-    hit: 1,
-    level: 1,
-    parent: null,
-    children: [],
-  }
-  var result = linker(node)
+  var ast = [
+    {
+      id: 's-1-1-1',
+      key: 'hello',
+      value: 'user',
+      mode: 'sync',
+      block: 1,
+      line: 1,
+      hit: 1,
+      level: 1,
+      parent: null,
+      children: [],
+    },
+  ]
 
-  // New linked data
-  t.equal(result.next, null)
-  t.equal(result.previous, null)
-  t.deepEqual(result.siblings, [])
-  t.equal(result.index, 0)
+  var result = linker(ast)
+
+  t.equal(result[0].next, null)
+  t.equal(result[0].previous, null)
+  t.deepEqual(result[0].siblings, [])
+  t.equal(result[0].index, 0)
 })
 
 test('single child', async ({ t }) => {
-  var node = {
-    id: 's-1-1-1',
-    key: '@p',
-    value: {},
-    mode: 'sync',
-    block: 1,
-    line: 1,
-    hit: 1,
-    level: 1,
-    parent: null,
-    children: [
-      {
-        id: 's-1-1-2',
-        key: '@span',
-        value: 'a',
-        mode: 'sync',
-        block: 1,
-        line: 1,
-        hit: 2,
-        level: 1,
-        children: [],
-      },
-    ],
-  }
-  var result = linker(node)
+  var ast = [
+    {
+      id: 's-1-1-1',
+      key: '@p',
+      value: {},
+      mode: 'sync',
+      block: 1,
+      line: 1,
+      hit: 1,
+      level: 1,
+      parent: null,
+      children: [
+        {
+          id: 's-1-1-2',
+          key: '@span',
+          value: 'a',
+          mode: 'sync',
+          block: 1,
+          line: 1,
+          hit: 2,
+          level: 1,
+          children: [],
+        },
+      ],
+    },
+  ]
 
-  // New linked data
-  t.equal(result.next, null)
-  t.equal(result.previous, null)
-  t.deepEqual(result.siblings, [])
-  t.equal(result.index, 0)
+  var result = linker(ast)
 
-  var child = result.children[0]
+  t.equal(result[0].next, null)
+  t.equal(result[0].previous, null)
+  t.deepEqual(result[0].siblings, [])
+  t.equal(result[0].index, 0)
+
+  var child = result[0].children[0]
 
   t.equal(child.next, null)
   t.equal(child.previous, null)
-  t.deepEqual(child.siblings, result.children)
+  t.deepEqual(child.siblings, result[0].children)
   t.equal(child.siblings[0].id, 's-1-1-2')
   t.equal(child.siblings[0].key, '@span')
   t.equal(child.index, 0)
 })
 
 test('siblings children', async ({ t }) => {
-  var node = {
-    id: 's-1-1-1',
-    key: '@p',
-    value: {},
-    mode: 'sync',
-    block: 1,
-    line: 1,
-    hit: 1,
-    level: 1,
-    parent: null,
-    children: [
-      {
-        id: 's-1-1-2',
-        key: '@span',
-        value: 'a',
-        mode: 'sync',
-        block: 1,
-        line: 1,
-        hit: 2,
-        level: 1,
-        children: [],
-      },
-      {
-        id: 's-1-2-2',
-        key: '@span',
-        value: 'a',
-        mode: 'sync',
-        block: 1,
-        line: 2,
-        hit: 2,
-        level: 1,
-        children: [],
-      },
-    ],
-  }
-  var result = linker(node)
+  var ast = [
+    {
+      id: 's-1-1-1',
+      key: '@p',
+      value: {},
+      mode: 'sync',
+      block: 1,
+      line: 1,
+      hit: 1,
+      level: 1,
+      parent: null,
+      children: [
+        {
+          id: 's-1-1-2',
+          key: '@span',
+          value: 'a',
+          mode: 'sync',
+          block: 1,
+          line: 1,
+          hit: 2,
+          level: 1,
+          children: [],
+        },
+        {
+          id: 's-1-2-2',
+          key: '@span',
+          value: 'a',
+          mode: 'sync',
+          block: 1,
+          line: 2,
+          hit: 2,
+          level: 1,
+          children: [],
+        },
+      ],
+    },
+  ]
 
-  // New linked data
-  t.equal(result.next, null)
-  t.equal(result.previous, null)
-  t.deepEqual(result.siblings, [])
-  t.equal(result.index, 0)
+  var result = linker(ast)
 
-  var fstChild = result.children[0]
+  t.equal(result[0].next, null)
+  t.equal(result[0].previous, null)
+  t.deepEqual(result[0].siblings, [])
+  t.equal(result[0].index, 0)
 
-  t.equal(fstChild.next.id, 's-1-2-2')
-  t.equal(fstChild.next.key, '@span')
-  t.equal(fstChild.previous, null)
-  t.deepEqual(fstChild.siblings, result.children)
-  t.equal(fstChild.siblings[0].id, 's-1-1-2')
-  t.equal(fstChild.siblings[1].id, 's-1-2-2')
-  t.equal(fstChild.index, 0)
+  var child1 = result[0].children[0]
 
-  var sndChild = result.children[1]
+  t.equal(child1.next.id, 's-1-2-2')
+  t.equal(child1.next.key, '@span')
+  t.equal(child1.previous, null)
+  t.deepEqual(child1.siblings, result[0].children)
+  t.equal(child1.siblings[0].id, 's-1-1-2')
+  t.equal(child1.siblings[1].id, 's-1-2-2')
+  t.equal(child1.index, 0)
 
-  t.equal(sndChild.next, null)
-  t.equal(sndChild.previous.id, 's-1-1-2')
-  t.equal(sndChild.previous.key, '@span')
-  t.deepEqual(sndChild.siblings, result.children)
-  t.equal(sndChild.siblings[0].id, 's-1-1-2')
-  t.equal(sndChild.siblings[1].id, 's-1-2-2')
-  t.equal(sndChild.index, 1)
+  var child2 = result[0].children[1]
+
+  t.equal(child2.next, null)
+  t.equal(child2.previous.id, 's-1-1-2')
+  t.equal(child2.previous.key, '@span')
+  t.deepEqual(child2.siblings, result[0].children)
+  t.equal(child2.siblings[0].id, 's-1-1-2')
+  t.equal(child2.siblings[1].id, 's-1-2-2')
+  t.equal(child2.index, 1)
 })
 
 test('nested children', async ({ t }) => {
-  var node = {
-    id: 's-1-1-1',
-    key: '@p',
-    value: {},
-    mode: 'sync',
-    block: 1,
-    line: 1,
-    hit: 1,
-    level: 1,
-    parent: null,
-    children: [
-      {
-        id: 's-1-1-2',
-        key: '@span',
-        value: 'a',
-        mode: 'sync',
-        block: 1,
-        line: 1,
-        hit: 2,
-        level: 1,
-        children: [],
-      },
-      {
-        id: 's-1-2-2',
-        key: '@span',
-        value: 'a',
-        mode: 'sync',
-        block: 1,
-        line: 2,
-        hit: 2,
-        level: 1,
-        children: [
-          {
-            id: 's-1-2-3',
-            key: '@a',
-            value: 'a',
-            mode: 'sync',
-            block: 1,
-            line: 1,
-            hit: 2,
-            level: 3,
-            children: [],
-          },
-          {
-            id: 's-1-3-3',
-            key: '@a',
-            value: 'a',
-            mode: 'sync',
-            block: 1,
-            line: 1,
-            hit: 2,
-            level: 1,
-            children: [],
-          },
-        ],
-      },
-    ],
-  }
-  var result = linker(node)
+  var ast = [
+    {
+      id: 's-1-1-1',
+      key: '@p',
+      value: {},
+      mode: 'sync',
+      block: 1,
+      line: 1,
+      hit: 1,
+      level: 1,
+      parent: null,
+      children: [
+        {
+          id: 's-1-1-2',
+          key: '@span',
+          value: 'a',
+          mode: 'sync',
+          block: 1,
+          line: 1,
+          hit: 2,
+          level: 1,
+          children: [],
+        },
+        {
+          id: 's-1-2-2',
+          key: '@span',
+          value: 'a',
+          mode: 'sync',
+          block: 1,
+          line: 2,
+          hit: 2,
+          level: 1,
+          children: [
+            {
+              id: 's-1-2-3',
+              key: '@a',
+              value: 'a',
+              mode: 'sync',
+              block: 1,
+              line: 1,
+              hit: 2,
+              level: 3,
+              children: [],
+            },
+            {
+              id: 's-1-3-3',
+              key: '@a',
+              value: 'a',
+              mode: 'sync',
+              block: 1,
+              line: 1,
+              hit: 2,
+              level: 1,
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ]
 
-  // New linked data
-  t.equal(result.next, null)
-  t.equal(result.previous, null)
-  t.deepEqual(result.siblings, [])
-  t.equal(result.index, 0)
+  var result = linker(ast)
 
-  var fstChild = result.children[0]
+  t.equal(result[0].next, null)
+  t.equal(result[0].previous, null)
+  t.deepEqual(result[0].siblings, [])
+  t.equal(result[0].index, 0)
 
-  t.equal(fstChild.next.id, 's-1-2-2')
-  t.equal(fstChild.next.key, '@span')
-  t.equal(fstChild.previous, null)
-  t.deepEqual(fstChild.siblings, result.children)
-  t.equal(fstChild.siblings[0].id, 's-1-1-2')
-  t.equal(fstChild.siblings[1].id, 's-1-2-2')
-  t.equal(fstChild.index, 0)
+  var child1 = result[0].children[0]
 
-  var sndChild = result.children[1]
+  t.equal(child1.next.id, 's-1-2-2')
+  t.equal(child1.next.key, '@span')
+  t.equal(child1.previous, null)
+  t.deepEqual(child1.siblings, result[0].children)
+  t.equal(child1.siblings[0].id, 's-1-1-2')
+  t.equal(child1.siblings[1].id, 's-1-2-2')
+  t.equal(child1.index, 0)
 
-  t.equal(sndChild.next, null)
-  t.equal(sndChild.previous.id, 's-1-1-2')
-  t.equal(sndChild.previous.key, '@span')
-  t.deepEqual(sndChild.siblings, result.children)
-  t.equal(sndChild.siblings[0].id, 's-1-1-2')
-  t.equal(sndChild.siblings[1].id, 's-1-2-2')
-  t.equal(sndChild.index, 1)
+  var child2 = result[0].children[1]
 
-  var fstGrandchild = sndChild.children[0]
+  t.equal(child2.next, null)
+  t.equal(child2.previous.id, 's-1-1-2')
+  t.equal(child2.previous.key, '@span')
+  t.deepEqual(child2.siblings, result[0].children)
+  t.equal(child2.siblings[0].id, 's-1-1-2')
+  t.equal(child2.siblings[1].id, 's-1-2-2')
+  t.equal(child2.index, 1)
 
-  t.equal(fstGrandchild.next.id, 's-1-3-3')
-  t.equal(fstGrandchild.next.key, '@a')
-  t.equal(fstGrandchild.previous, null)
-  t.deepEqual(fstGrandchild.siblings, sndChild.children)
-  t.equal(fstGrandchild.siblings[0].id, 's-1-2-3')
-  t.equal(fstGrandchild.siblings[1].id, 's-1-3-3')
-  t.equal(fstGrandchild.index, 0)
+  var grandchild1 = child2.children[0]
 
-  var sndGrandchild = sndChild.children[1]
+  t.equal(grandchild1.next.id, 's-1-3-3')
+  t.equal(grandchild1.next.key, '@a')
+  t.equal(grandchild1.previous, null)
+  t.deepEqual(grandchild1.siblings, child2.children)
+  t.equal(grandchild1.siblings[0].id, 's-1-2-3')
+  t.equal(grandchild1.siblings[1].id, 's-1-3-3')
+  t.equal(grandchild1.index, 0)
 
-  t.equal(sndGrandchild.next, null)
-  t.equal(sndGrandchild.previous.id, 's-1-2-3')
-  t.equal(sndGrandchild.previous.key, '@a')
-  t.deepEqual(sndGrandchild.siblings, sndChild.children)
-  t.equal(sndGrandchild.siblings[0].id, 's-1-2-3')
-  t.equal(sndGrandchild.siblings[1].id, 's-1-3-3')
-  t.equal(sndGrandchild.index, 1)
+  var grandchild2 = child2.children[1]
+
+  t.equal(grandchild2.next, null)
+  t.equal(grandchild2.previous.id, 's-1-2-3')
+  t.equal(grandchild2.previous.key, '@a')
+  t.deepEqual(grandchild2.siblings, child2.children)
+  t.equal(grandchild2.siblings[0].id, 's-1-2-3')
+  t.equal(grandchild2.siblings[1].id, 's-1-3-3')
+  t.equal(grandchild2.index, 1)
 })
