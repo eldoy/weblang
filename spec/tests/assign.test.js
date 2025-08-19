0d2d5d30dbf71a8522c156b56ca4310ddcd2b2a9
Vidar Eldøy <vidar@eldoy.com>
2025-08-19 05:01:52 +0200

    fix: setup run

--- spec/tests/assign.test.js
+++
@@ -1,9 +0,0 @@
-// var assign = require('../../lib/assign.js')
-
-// test('string', async ({ t }) => {
-//   var state = {
-//     vars: {},
-//   }
-//   assign('=hello: world', state)
-//   t.equal(state.vars.hello, 'world')
-// })
--- spec/tests/run.test.js
+++ spec/tests/run.test.js
@@ -1,7 +1,16 @@
+var compile = require('../../lib/compile.js')
 var run = require('../../lib/run.js')
 
 test('empty', async ({ t }) => {
-  var result = run({})
-  var expect = {}
-  t.deepEqual(result, expect)
+  var ast = compile('')
+  var output = run(ast)
+  t.equal(output.state.result, null)
+  t.equal(output.state.err, null)
+})
+
+test('value', async ({ t }) => {
+  var ast = compile('a: hello')
+  var output = run(ast)
+  t.equal(output.state.result, null)
+  t.equal(output.state.err, null)
 })
