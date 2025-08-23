87f2159425421c435d2e82d32898c5e241b7075c
Vidar Eldøy <vidar@eldoy.com>
2025-08-23 04:36:50 +0200

    fix: generalize set get

--- spec/tests/set.test.js
+++ spec/tests/set.test.js
@@ -2,6 +2,6 @@ var set = require('../../lib/set.js')
 
 test('value', async ({ t }) => {
   var state = { vars: {} }
-  var result = set(state, 'hello', 'world')
+  var result = set(state.vars, 'hello', 'world')
   t.equal(state.vars.hello, 'world')
 })
