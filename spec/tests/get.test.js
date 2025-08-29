d78d607268680dddc5721d8f655342b499aef965
Vidar Eldøy <vidar@eldoy.com>
2025-08-29 08:07:02 +0200

    fix: prepare scoped functions

--- spec/tests/get.test.js
+++ spec/tests/get.test.js
@@ -2,6 +2,6 @@ var get = require('../../lib/get.js')
 
 test('value', async ({ t }) => {
   var state = { vars: { hello: 'world' } }
-  var data = get(state.vars, 'hello')
+  var data = get(state, 'hello')
   t.equal(data, 'world')
 })
--- spec/tests/set.test.js
+++ spec/tests/set.test.js
@@ -2,6 +2,6 @@ var set = require('../../lib/set.js')
 
 test('data', async ({ t }) => {
   var state = { vars: {} }
-  var result = set(state.vars, 'hello', 'world')
+  var result = set(state, 'hello', 'world')
   t.equal(state.vars.hello, 'world')
 })
