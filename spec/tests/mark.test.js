6d2ddf1c6f1ce926c27dbf7cfdcb4f95bf2dff92
Vidar Eldøy <vidar@eldoy.com>
2025-08-30 14:09:58 +0200

    fix: rename tag to mark

--- spec/tests/mark.test.js
+++ spec/tests/mark.test.js
@@ -1,57 +1,57 @@
-var tag = require('../../lib/tag.js')
+var mark = require('../../lib/mark.js')
 
 test('assign', async ({ t }) => {
   var code = '=hello: user'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '=hello_ID_s-1-1-1-1_ID_: user'
   t.equal(result, expect)
 })
 
 test('assign - async', async ({ t }) => {
   var code = '- =hello: user'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '=hello_ID_a-1-1-3-1_ID_: user'
   t.equal(result, expect)
 })
 
 test('assign - async spaced', async ({ t }) => {
   var code = '-   =hello: user'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '=hello_ID_a-1-1-5-1_ID_: user'
   t.equal(result, expect)
 })
 
 test('assign func', async ({ t }) => {
   var code = '=hello@func: user'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '=hello@func_ID_s-1-1-7-1_ID_: user'
   t.equal(result, expect)
 })
 
 test('assign func - async', async ({ t }) => {
   var code = '- =hello@func: user'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '=hello@func_ID_a-1-1-9-1_ID_: user'
   t.equal(result, expect)
 })
 
 test('func - one', async ({ t }) => {
   var code = '@db: {}'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '@db_ID_s-1-1-1-1_ID_: {}'
   t.equal(result, expect)
 })
 
 test('func - multiple', async ({ t }) => {
   var code = '@p: { @p: a }'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '@p_ID_s-1-1-1-1_ID_: { @p_ID_s-1-1-7-2_ID_: a }'
   t.equal(result, expect)
 })
 
 test('func - async single', async ({ t }) => {
   var code = '- @p: { @p: a }'
-  var result = tag(code, 1, 1)
+  var result = mark(code, 1, 1)
   var expect = '@p_ID_a-1-1-3-1_ID_: { @p_ID_s-1-1-9-2_ID_: a }'
   t.equal(result, expect)
 })
